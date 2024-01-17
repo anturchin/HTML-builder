9cd73fac8fc7da676f914cd12481ea740dfcae39
anturchin <tur4intwo@gmail.com>
2024-01-17 14:06:13 +0300

    refactor: update task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,28 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const pathFolder = path.resolve(__dirname + '/secret-folder');
+const pathFolderSecret = path.resolve(__dirname + '/secret-folder');
 
-const displayFileInfo = async () => {
+const displayFileInfo = async (pathFolder) => {
   try {
     const files = await fs.readdir(pathFolder, { withFileTypes: true });
 
     for (const file of files) {
-      if (file.isFile()) {
-        const fileName = file.name.split('.')[0];
-        const fileExt = path.extname(file.name).slice(1);
+      if (file.isDirectory()) {
+        const newPath = path.resolve(pathFolder, file.name);
+        await displayFileInfo(newPath);
+      } else if (file.isFile()) {
+        const fileNameParts = file.name.split('.');
+        const newFileName = fileNameParts.slice(0, -1).join('.');
+        const fileExt = fileNameParts.length > 1 ? fileNameParts.pop() : '';
         const filePath = path.resolve(pathFolder, file.name);
         const fileStats = await fs.stat(filePath);
         const fileSize = fileStats.size / 1024;
-
-        console.log(`${fileName} - ${fileExt} - ${fileSize.toFixed(3)}kb`);
+        console.log(
+          `${
+            newFileName ? newFileName : fileExt
+          } - ${fileExt} - ${fileSize.toFixed(3)}kb`,
+        );
       }
     }
   } catch (err) {
@@ -23,4 +30,4 @@ const displayFileInfo = async () => {
   }
 };
 
-displayFileInfo();
+displayFileInfo(pathFolderSecret);
